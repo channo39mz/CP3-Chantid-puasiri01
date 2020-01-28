1a42bc317dcd50b9b2f0361b8fcc740d6becafd1
channo39mz <60224796+channo39mz@users.noreply.github.com>
2020-01-28 22:19:17 +0700

    Update and rename README.md to Exercise4_Chantid_P.py

---
+++ Exercise4_Chantid_P.py
@@ -0,0 +1,17 @@
+# CP3-Chantid-puasiri01
+Exercise4_Chantid_P.py
+
+FE = 70.00
+'''Foundation English'''
+GB = 80.00
+'''General Business'''
+ItCS = 75.5
+'''Introduction to Computer Systems'''
+CP = 90.00
+'''Computer Programming'''
+
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :", ItCS)
+print("Computer Programming :",CP)
--- README.md
+++
@@ -1,2 +0,0 @@
-# CP3-Chantid-puasiri01
-Exercise4_Chantid_P.py
